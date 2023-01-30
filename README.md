d61fe31db5c732ce55128d98e27dce15ca5bfd63
Dukenson Dodor <104813218+dodor101@users.noreply.github.com>
2023-01-29 23:53:42 -0600

    added functionalities for the readme generator app

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# readme-generator
+# README GENERATOR
+
+## Description 
+This is application was build to make the process of creating readme somewhat easier and save time. Please give it a try. 
+
+## Technologies used 
+I used Nodejs to build this application all the prompts done using the inquirer dependencies. 
+
+## Questions
+* Email: dodordickenson21@gmail.com
+* Github:
+[Guithub](https://github.com/dodor101/readme-generator)
---
+++ index.js
@@ -0,0 +1,101 @@
+// External packages 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// inquirer to generate qustions
+
+inquirer.prompt(
+  [
+    {
+      type: 'input',
+      message: 'What the project title?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Describe Your application?',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'How to install your app?',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Instructions to follow',
+      name: 'instructions',
+    },
+    {
+      type: 'input',
+      message: 'How to use your App?',
+      name: 'usage',
+    },
+    {
+      type: 'list',
+      message: 'What license did you use?',
+      name: 'license',
+      choices: ['the MIT License', 'The GPL License', 'Apache License', 'N/A'],
+    },
+    {
+      type: 'input',
+      message: 'Github username',
+      name: 'git',
+    },
+    {
+      type: 'input',
+      message: 'E-mail:',
+      name: 'email',
+    },
+  ]
+).then(({ title,
+  description,
+  installation,
+  instructions,
+  license,
+  git,
+  email,
+  usage
+}) => {
+  const template = `# ${title}
+  
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Instruction](#instruction)
+
+  ## Description
+  ${description}
+
+  ## Installation
+  ${installation}
+
+  ## Usage 
+  ${usage}
+
+  ## Instructions
+  ${instructions} 
+
+  ## License
+  ${license}
+  
+  # Questions 
+  * Github: [github](https://github.com/${git}) 
+  * E-mail: ${email}
+  `
+  // function to create new file
+  createNewFile(title, template);
+
+})
+
+// creating the createNewFile function 
+function createNewFile(fileName, data) {
+  fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.md`, data, (err) => {
+    if (err) {
+      console.log(err)
+    }
+    console.log('Your README has been generated');
+  })
+}
+
+// Let's install our packages
